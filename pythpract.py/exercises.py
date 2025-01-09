f06ca4d0464fc86c251a210da94e452ce34ae61d
1dollar5tokens <willardthsmith@gmail.com>
2025-01-09 09:32:33 -0800

    done homework py first day

---
+++ pythpract.py/exercises.py
@@ -0,0 +1,19 @@
+names = (
+    'asta', 
+    'butterscotch', 
+    'pudding', 
+    'neptune', 
+    'darwin'
+    )
+print(names)
+
+pets = {
+    'asta': 'dog', 
+    'butterscotch': 'cat', 
+    'pudding': 'cat', 
+    'neptune': 'fish', 
+    'darwin': 'lizard',
+    
+}
+
+print(pets)
