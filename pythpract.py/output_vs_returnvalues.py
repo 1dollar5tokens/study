49300cdfa5dcca0f23479ea811dcb9df99709eea
1dollar5tokens <willardthsmith@gmail.com>
2025-01-12 09:58:15 -0800

    more fun

---
+++ pythpract.py/output_vs_returnvalues.py
@@ -0,0 +1,10 @@
+print('abc')
+
+list(range(3))
+
+>>> print('abc')  # output spits out what you put in. print abc get abc
+abc
+>>> list(range(3))  # return spits out an answer or Python has to calculate 
+[0, 1, 2]
+>>>
+ 
