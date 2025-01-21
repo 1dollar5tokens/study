608bbe57fd74d58b138adcbb6c4d0e2fd25292b3
1dollar5tokens <willardthsmith@gmail.com>
2025-01-21 11:02:34 -0800

    creating functions

---
+++ pythpract.py/functions_methods/creating_functions.py
@@ -0,0 +1,48 @@
+#def func_name():
+    #func_body
+ 
+##trying to understand whats going on
+    
+# def say():
+#     print('Hi') 
+    
+# say()    
+
+##next example to help me get it 
+
+# def say():
+#     print('output from say')
+    
+# print('first')          ##prints like normal
+# say()                   ##it calls to the function of say()
+# print('last')
+# print('willardth')
+# print('smith')
+# say()                   ##it calls to the function of say()
+# print('delon')
+
+# #kindof got it
+
+# def say():
+#     """
+#     The say function prints "Hi!"
+#     """
+#     print('Hi')
+    
+    
+# print('-' * 60)
+# print(say.__doc__)
+# print('_' * 60)
+# help(say)
+
+# #output
+
+# ------------------------------------------------------------
+
+#     The say function prints "Hi!"
+
+# ____________________________________________________________
+# Help on function say in module __main__:
+
+# say()
+#     The say function prints "Hi!"
